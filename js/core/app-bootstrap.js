85633f407d5b171e2bb7162506ee775056cf6ec9
millux <markus@marsrakete.de>
2026-08-26 06:03:15 +0200

    version bump für neuen bootstrap

--- js/core/app-bootstrap.js
+++ js/core/app-bootstrap.js
@@ -1,6 +1,6 @@
 const FALLBACK_APP_VERSION_INFO = Object.freeze({
-  appVersion: "0.4.272",
-  cacheVersion: "v291",
+  appVersion: "0.4.273",
+  cacheVersion: "v292",
   label: "Fix Thread Explorer reply handoff to composer",
 });
 
--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 globalThis.APP_VERSION_INFO = Object.freeze({
-  appVersion: "0.4.272",
-  cacheVersion: "v291",
+  appVersion: "0.4.273",
+  cacheVersion: "v292",
   label: "Fix Thread Explorer reply handoff to composer",
 });
